4595626a1bddd7ad798cf525be7450287118c5ea
Heiler Nova <=>
2022-10-23 15:58:22 -0500

    Se mofica la carga para crear el archivo env.json en caso de no existir

--- src/Api/Functions/nv_api_load_config.php
+++ src/Api/Functions/nv_api_load_config.php
@@ -5,8 +5,34 @@ function nv_api_load_config(): void
     $dir = $_ENV['nv']['api']['dir'];
 
     $dir_config = "$dir/env.json";
+    
+    if (!file_exists("$dir/index.json")) throw new Exception("No se encotro el index.json");
 
-    if (!file_exists($dir_config)) throw new Exception("Falta establecer el archivo de las condiguraciones del [config/env.json]");
+    $index_config = json_decode(file_get_contents("$dir/index.json"), true);
+    if (is_null($index_config)) throw new Exception("Error con el contenido del index.json");
+
+    if (!file_exists($dir_config)) {
+        $json['timezone'] = $index_config['databases'] ?? 'UTC';
+
+        $dbs = $index_config['databases'];
+
+        foreach ($dbs as $name => $db) {
+            $json['databases'][$name]['type'] = $db['type'];
+            $json['databases'][$name]['connectionData'] = [
+                "hostname" => "localhost",
+                "username" => "root",
+                "password" => "",
+                "database" => "test",
+                "post" => null
+            ];
+        }
+
+        $f = fopen("$dir/env.json", 'a');
+        fputs($f, json_encode($json, 128));
+        fclose($f);
+
+        throw new Exception("Falta establecer el archivo de las condiguraciones del [env.json]");
+    }
 
     $_ENV['nv']['api']['config'] = json_decode(file_get_contents($dir_config), true);
 
