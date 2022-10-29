ac09a7e821f6165f986236ae4c266eff204d3cf4
Heiler Nova <=>
2022-10-28 20:32:14 -0500

    Se agrega el parametro files a la clase HttpRequest

--- src/Http/HttpRequest.php
+++ src/Http/HttpRequest.php
@@ -5,10 +5,12 @@ class HttpRequest
 {
     public readonly string $url;
     public readonly string $method;
-    public readonly string $ip;
     public readonly mixed $body;
+    /** @var HttpFile[] */
+    public readonly array $files;
     public readonly array $params;
     public readonly array $paramsURL;
+    public readonly string $ip;
     public readonly string $device;
     public readonly string $platform;
 
@@ -18,6 +20,7 @@ class HttpRequest
         $this->method = $_ENV['nvx']['request']['method'];
         $this->ip = $_ENV['nvx']['request']['ip'];
         $this->body = $_ENV['nvx']['request']['body'];
+        $this->files = $_ENV['nvx']['request']['files'];
         $this->params = $_ENV['nvx']['request']['params'];
         $this->paramsURL = $_ENV['nvx']['request']['query-params'];
         $this->platform = $_ENV['nvx']['request']['platform'];
